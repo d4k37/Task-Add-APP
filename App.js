06b2f7c4fb0f4c4ef6fb58b05f18770c2f61e029
Tony Correia <luis.dhac@gmail.com>
2022-08-01 13:56:19 -0300

    React-native setup and states

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react'
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+
+              const [todoItem, setTodoItem] = useState('')
+              const [todoList, setTodoList] = useState([])
+
+              const addTodoList = () => {
+                setTodoList([...todoList, todoItem])
+                console.log(todoList);
+              }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <View>
+       <TextInput 
+       placeholder='Entre a task a se fazer'
+       style={styles.textInput}
+       onChangeText={text => setTodoItem(text)}
+       value={todoItem}
+       />
+       <Button  
+       title='Adicione a task'
+       onPress={addTodoList}
+       />
+     </View>
+     <View>
+       <Text>Lista de tasks</Text>
+     </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 60,
   },
+  textInput:{
+    padding: 10,
+    borderColor: 'black',
+    marginBottom: 10,
+    borderWidth: 1
+  }
 });
